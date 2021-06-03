a804bf25ea733e2d5b84f843bc3901c037315d70
Caique_Ramos <code@rino3.com.br>
2021-06-02 21:55:20 -0400

    mudança

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -5,7 +5,6 @@ const User = require('./user')
 const env = require('../../.env')
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
-
 const sendErrorsFromDB = (res, dbErrors) => {
     const errors = []
     _.forIn(dbErrors.errors, error => errors.push(error.message))
